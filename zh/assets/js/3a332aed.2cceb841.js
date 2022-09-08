908c731c4ea5baa6c3ff6b32e28b95f4466446b6
Twwy <Twwy@users.noreply.github.com>
2022-09-08 10:33:16 +0000

    change base url

--- zh/assets/js/3a332aed.2cceb841.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksreworks=self.webpackChunksreworks||[]).push([[8800],{99567:e=>{e.exports=JSON.parse('{"permalink":"/zh/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":3,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
